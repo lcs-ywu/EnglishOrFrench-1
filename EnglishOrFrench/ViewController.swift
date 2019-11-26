edead1f52439db870019db26ba96fce79b8d5630
Scott Doggett <sdoggett@lcs.on.ca>
2019-11-26 10:53:37 -0500

    finished user interface and unwrap text

--- EnglishOrFrench/ViewController.swift
+++ EnglishOrFrench/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var messageInput: UITextView!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    //MARK: Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func analyzeButton(_ sender: Any) {
+        
+        guard let phraseInput = messageInput.text, !phraseInput.isEmpty else {
+            outputLabel.text = "Enter a Message"
+            return
+        }
+        
+        if phraseInput.count == 0 {
+            outputLabel.text = "Please enter a message"
+            return
+        }
+        
+        
+        
+    }
+    
 }
-
