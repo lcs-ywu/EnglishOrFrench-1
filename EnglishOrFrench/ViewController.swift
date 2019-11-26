85ffb3af75ebe2837cfbaa4d4c69ad5fb2f84b4b
Scott Doggett <sdoggett@lcs.on.ca>
2019-11-26 10:44:19 -0500

    Initial Commit

---
+++ EnglishOrFrench/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnglishOrFrench
+//
+//  Created by Doggett, Scott on 2019-11-26.
+//  Copyright © 2019 Doggett, Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
